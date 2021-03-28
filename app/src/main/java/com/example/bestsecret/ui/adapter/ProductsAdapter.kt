1fefa8e385a4c6ea7327ed7db96bc839b0e4721a
Cristian <crisgutierrez014@gmail.com>
2021-03-28 17:39:46 +0200

    Implemented product details screen.
    - Created product details screen and product details viewModel
    - Added navigation from products to product detail when a product is selected
    - Created dialog for feature not available.
    - Added add to car to the product item with the same logic that we have in the product details

--- app/src/main/java/com/example/bestsecret/ui/adapter/ProductsAdapter.kt
+++ app/src/main/java/com/example/bestsecret/ui/adapter/ProductsAdapter.kt
@@ -15,7 +15,10 @@ import com.example.bestsecret.ext.loadFromUrl
 import com.example.bestsecret.utils.formatNumberToTwoDigits
 import com.example.bestsecret.utils.getPriceWithDiscount
 
-class ProductsAdapter() : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+class ProductsAdapter(
+    private val onProductClick: (Product) -> Unit,
+    private val onAddToCartClick: () -> Unit
+) : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
 
     private var productList: List<Product> = emptyList()
     private lateinit var context: Context
@@ -28,22 +31,29 @@ class ProductsAdapter() : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = productList[position]
-        holder.productImage.loadFromUrl(item.image)
-        holder.productName.text = item.name
-        holder.productPrice.text = context.getString(R.string.product_item_price, item.currency, getPriceWithDiscount(item.price, item.discountPercentage))
+        val product = productList[position]
+        holder.productImage.loadFromUrl(product.image)
+        holder.productName.text = product.name
+        holder.productPrice.text = context.getString(R.string.product_item_price, getPriceWithDiscount(product.price, product.discountPercentage), product.currency)
 
         // productOldPrice is only visible if the discount is not 0
-        holder.productOldPrice.isVisible = item.discountPercentage != 0
-        holder.productOldPrice.text = context.getString(R.string.product_item_old_price, item.currency, formatNumberToTwoDigits(item.price.toDouble()))
+        holder.productOldPrice.isVisible = product.discountPercentage != 0
+        holder.productOldPrice.text = context.getString(R.string.product_item_old_price, formatNumberToTwoDigits(product.price.toDouble()), product.currency)
         holder.productOldPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
 
         // productDiscount is only visible if the discount is not 0
-        holder.productDiscount.isVisible = item.discountPercentage != 0
-        holder.productDiscount.text = context.getString(R.string.product_item_discount, item.discountPercentage)
+        holder.productDiscount.isVisible = product.discountPercentage != 0
+        holder.productDiscount.text = context.getString(R.string.product_item_discount, product.discountPercentage)
 
         // Set favorite button click listener
         holder.productFavorite.setOnClickListener { holder.productFavorite.isSelected = !holder.productFavorite.isSelected }
+
+        // Set Add to cart button, this button is only visible if we have stock.
+        holder.productAddToCart.isVisible = product.stock > 0
+        holder.productAddToCart.setOnClickListener { onAddToCartClick.invoke() }
+
+        // Set item listener
+        holder.itemView.setOnClickListener { onProductClick(product) }
 
     }
 
@@ -57,6 +67,7 @@ class ProductsAdapter() : RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val productImage: ImageView = view.findViewById(R.id.product_item_image)
         val productFavorite: ImageView = view.findViewById(R.id.product_item_favorite)
+        val productAddToCart: ImageView = view.findViewById(R.id.product_item_add_to_cart)
         val productName: TextView = view.findViewById(R.id.product_item_name)
         val productOldPrice: TextView = view.findViewById(R.id.product_item_old_price)
         val productPrice: TextView = view.findViewById(R.id.product_item_price)
--- app/src/main/java/com/example/bestsecret/ui/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/com/example/bestsecret/ui/viewmodel/ProductsViewModel.kt
@@ -5,6 +5,7 @@ import com.example.bestsecret.domain.model.Product
 import com.example.bestsecret.domain.model.ProductQueryParam
 import com.example.bestsecret.domain.state.DataState
 import com.example.bestsecret.domain.usecase.GetAllProductsUseCase
+import com.example.bestsecret.utils.PRODUCTS_PAGE_SIZE
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -21,18 +22,29 @@ constructor(
     private val _dataState: MutableLiveData<DataState<List<Product>>> = MutableLiveData()
     val dataState: LiveData<DataState<List<Product>>> = _dataState
 
+    // region PUBLIC METHODS -----------------------------------------------------------------------
     fun setStateEvent(mainStateEvent: MainStateEvent) {
         viewModelScope.launch {
             when(mainStateEvent) {
                 is MainStateEvent.GetAllProductsEvent -> {
-                    getAllProductsUseCase.getProducts(ProductQueryParam.QueryAllProducts(1,5))
-                        .onEach {
-                            _dataState.value = it
-                        }.launchIn(viewModelScope)
+                    getAllProducts(1) // Todo remove hardcoded value when the BE is working and implement pagination.
                 }
             }
         }
     }
+    // endregion
+
+    // region PRIVATE METHODS -----------------------------------------------------------------------
+    /**
+     * Get all the products for the selected [page]
+     */
+    private suspend fun getAllProducts(page: Int) {
+        getAllProductsUseCase.getProducts(ProductQueryParam.QueryAllProducts(page, PRODUCTS_PAGE_SIZE))
+            .onEach {
+                _dataState.value = it
+            }.launchIn(viewModelScope)
+    }
+    // endregion
 
 }
 
